d85c0cd3a8c4b648dd2f82823a258fde9dd7f363
papercut <papercut@172.17.61.71>
2019-08-13 09:00:14 +0700

    1 commit

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+
+public class Student {
+    
+    // declare attribute
+    
+    protected String studentID;
+    protected String studentName;
+    protected String faculty;
+    protected String major;
+
+    public void enrollment() {
+        System.out.println("enrollment");
+    }
+
+    public void payment() {
+        System.out.println("payment by package");
+    }
+
+    public void addCours() {
+        System.out.println("addCours");
+    }
+
+    public void dropCourse() {
+        System.out.println("dropCourse");
+    }
+
+
+    
+}//end 
